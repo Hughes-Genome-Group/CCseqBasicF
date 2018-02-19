32077e297c92f3a2e3874a4cfc43664e8d9287a1
Jelena Telenius <jelena.telenius@gmail.com>
2018-02-19 11:04:35 +0000

    genomes.txt file portability bug fix

--- bin/runscripts/analyseMappedReads.pl
+++ bin/runscripts/analyseMappedReads.pl
@@ -952,7 +952,7 @@ genomesFile http://$public_url/$sample\_$version\_genomes.txt
 email $email";
 
 print TRACKHUBB "genome $genome
-trackDb http://sara.molbiol.ox.ac.uk$public_folder/$sample\_$version\_tracks.txt";
+trackDb http://$public_url/$sample\_$version\_tracks.txt";
 
 
 print REPORTFH "\nThe track hub can be found at:
